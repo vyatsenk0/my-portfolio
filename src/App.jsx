ffb0ba36b2445130e02ee4a96824d09059a7c3fa
Vladislav <92332562+vyatsenk0@users.noreply.github.com>
2025-07-09 17:25:50 -0400

    added header component & updated app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-
 import './App.css'
+
+import Header from './components/Header'
+import Home from './components/Home'
 
 function App() {
 
 
   return (
     <>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header />
+      <Home />
     </>
   )
 }
