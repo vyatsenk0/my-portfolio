421e1fe99edd73abd24f40bbbe8256b9caa69fe9
Vladislav <92332562+vyatsenk0@users.noreply.github.com>
2025-07-11 10:21:57 -0400

    remove mui library imports

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,58 +1,8 @@
-import Breadcrumbs from '@mui/material/Breadcrumbs';
-import Link from '@mui/material/Link';
-import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
-import Typography from '@mui/material/Typography';
+
 
 function Header() {
   return (
-    <Paper
-      elevation={3}
-      square
-      sx={{
-        position: 'fixed',
-        top: 0,
-        left: 0,
-        width: '100%',
-        zIndex: 1000,
-        backgroundColor: 'rgba(255, 255, 255, 0.9)',
-        backdropFilter: 'blur(6px)',
-        px: 4,
-        py: 1.5,
-        opacity: 0.6
-      }}
-    >
-      <Box
-        sx={{
-          maxWidth: '1200px',
-          margin: '0 auto',
-          display: 'flex',
-          justifyContent: 'space-between',
-          alignItems: 'center',
-        }}
-      >
-        {/* Left side: Name */}
-        <Typography variant="h6" fontWeight="bold">
-          Vladislav Yatsenko
-        </Typography>
-
-        {/* Right side: Breadcrumb links */}
-        <Breadcrumbs separator="/" aria-label="breadcrumb">
-          <Link underline="hover" color="inherit" href="#Home">
-            Home
-          </Link>
-          <Link underline="hover" color="inherit" href="#About">
-            About
-          </Link>
-          <Link underline="hover" color="inherit" href="#Portfolio">
-            Portfolio
-          </Link>
-          <Link underline="hover" color="inherit" href="#Contact">
-            Contact
-          </Link>
-        </Breadcrumbs>
-      </Box>
-    </Paper>
+   <h1>Header</h1>
   );
 }
 
---
+++ src/components/Portfolio.jsx
@@ -0,0 +1,14 @@
+//import './App.css'
+
+
+
+
+function Portfolio() {
+
+
+  return (
+    <h1>Portfolio</h1>
+  )
+}
+
+export default Portfolio
