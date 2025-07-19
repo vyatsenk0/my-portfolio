d83799dde9c478aed27f7d3ce427833dda684229
Vladislav <92332562+vyatsenk0@users.noreply.github.com>
2025-07-19 17:33:24 -0400

    style: adjusted Grid on footer (3/3)

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,12 +8,10 @@ export default function Footer() {
         <Grid> {/* 12 columns */}
             {/* LEFT COLUMN */}
             <Grid.Col span={3} offset={1}>
-                <Text mb={20} fw={600} size="xl">Vladislav Yatsenko</Text>
-                <Text  mb={20} size="md" c="dimmed">Full Stack Developer crafting seamless digital experiences. Passionate about clean code, elegant design, and building tools that make a difference.</Text>
                 <Button
                     onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
                     style={{
-                        marginTop: '10px',
+                        marginTop: '43px',
                         padding: '6px 12px',
                         borderRadius: '6px',
                         background: 'transparent',
@@ -30,28 +28,24 @@ export default function Footer() {
               </Button>
             </Grid.Col>
 
-             {/* RIGHT COLUMNS */}
-            <Grid.Col span={4} offset={3}>
-                <Grid style={{display: 'flex', alignItems: 'center', height: '100%'}}> {/* 12 columns */}
-                     {/* Working Time */}
-                     <Grid.Col span={6}>
-                        <Text fw={600} mb={20}>Working Time</Text>
-                        <Text size="md" c="dimmed">Almaty, Kazakhstan</Text>
-                        <Text size="md" c="dimmed">Monday - Friday</Text>
-                        <Text size="md" c="dimmed">09:00 AM – 06:00 PM (UTC +5)</Text>
-                     </Grid.Col>
-                     
-                    {/* Contact Info */}
-                    <Grid.Col span={5} offset={1}>
-                        <Text fw={600} mb={20}>Contact Info</Text>
-                        <Text size="md" c="dimmed">+1 (437) 421-1371</Text>
-                        <Text size="md" c="dimmed">yatsenkovladislav50@gmail.com</Text>
-                     </Grid.Col>
-                </Grid>
+            {/* CENTER COLUMN */}
+            {/* Working Time */}
+            <Grid.Col span={4}>
+              <Text fw={600} mb={20}>Working Time</Text>
+              <Text size="md" c="dimmed">Almaty, Kazakhstan</Text>
+              <Text size="md" c="dimmed">Monday - Friday</Text>
+              <Text size="md" c="dimmed">09:00 AM – 06:00 PM (UTC +5)</Text>
+            </Grid.Col>
+            
+            {/* RIGHT COLUMN */}
+            {/* Contact Info */}
+            <Grid.Col span={4}>
+              <Text fw={600} mb={20}>Contact Info</Text>
+              <Text size="md" c="dimmed">+1 (437) 421-1371</Text>
+              <Text size="md" c="dimmed">yatsenkovladislav50@gmail.com</Text>
             </Grid.Col>
         </Grid>
       
-
       {/* AFTER FOOTER */}
       <Container fluid className={classes.afterFooter}>
         <Text c="dimmed" size="md">
