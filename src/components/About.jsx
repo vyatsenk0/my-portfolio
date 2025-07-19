61404e81c3160f17dc1ee5dd175328828133fcc2
Vladislav <92332562+vyatsenk0@users.noreply.github.com>
2025-07-19 14:14:33 -0400

    style: aboutPage, section padding & icons bg transparency

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -25,7 +25,7 @@ export default function About() {
   ];
 
   return (
-    <Container size="lg" py="xl" id="about">
+    <Container size="lg" py="xl" id="about" style={{ padding: '100px 0' }}>
       <Title
         fz={50}
         order={1}
