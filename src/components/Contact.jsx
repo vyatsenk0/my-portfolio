794f2a1343f6cedeef554335471023d9b7e3643a
Vladislav <92332562+vyatsenk0@users.noreply.github.com>
2025-08-06 15:46:16 +0500

    refactor: Contact div containers; style: contact bg, paddding & borders

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,8 @@
-import { Button, Group, SimpleGrid, Textarea, TextInput, Title } from '@mantine/core';
+import { Button, Group, SimpleGrid, Textarea, TextInput, Title, Container, Box } from '@mantine/core';
 import { useForm } from '@mantine/form';
+import { IconSend } from '@tabler/icons-react';
+
+import classes from './Contact.module.css';
 
 export default function Contact() {
   const form = useForm({
@@ -15,53 +18,92 @@ export default function Contact() {
   });
 
   return (
-    <form id="contact" onSubmit={form.onSubmit(() => {})}>
-      <Title
-        order={2}
-        size="h1"
-        style={{ fontFamily: 'Outfit, var(--mantine-font-family)' }}
-        fw={900}
-        ta="center"
-      >
-        Get in touch
+    <Container className={classes.wrapper} id="contact">
+      <Title fz="50" order={1} mb="sm" style={{
+            background: 'linear-gradient(to right, #2172f4, #5b43d6, #9333ea)',
+            WebkitBackgroundClip: 'text',
+            WebkitTextFillColor: 'transparent',
+
+            }}>
+        Contact Me
       </Title>
+      <Title size="h6" style={{paddingBottom: '40px'}}>
+        Got something to ask? Reach out and I’ll get back to you in no time.
+      </Title>
+      <Container className={classes.container}>
+      
+        <form className={classes.form} onSubmit={form.onSubmit(() => {})}>
+        
 
-      <SimpleGrid cols={{ base: 1, sm: 2 }} mt="xl">
-        <TextInput
-          label="Name"
-          placeholder="Your name"
-          name="name"
-          variant="filled"
-          {...form.getInputProps('name')}
-        />
-        <TextInput
-          label="Email"
-          placeholder="Your email"
-          name="email"
-          variant="filled"
-          required
-          {...form.getInputProps('email')}
-        />
-      </SimpleGrid>
+          <Title fz="40" order={1} mb="sm" ta="left" style={{
+            background: 'linear-gradient(to right, #2172f4, #5b43d6, #9333ea)',
+            WebkitBackgroundClip: 'text',
+            WebkitTextFillColor: 'transparent',
 
-      <Textarea
-        mt="md"
-        label="Message"
-        placeholder="Your message"
-        maxRows={10}
-        minRows={5}
-        autosize
-        name="message"
-        variant="filled"
-        required
-        {...form.getInputProps('message')}
-      />
+            }}>
+            Get in touch
+          </Title>
 
-      <Group justify="center" mt="xl">
-        <Button type="submit" size="md">
-          Send message
-        </Button>
+          <Title size="h6"  ta="left">
+            Drop me a message and let’s start the conversation.
+          </Title>
+
+          <SimpleGrid cols={{ base: 1, sm: 2 }} mt="xl">
+            <TextInput
+              placeholder="Your name"
+              name="name"
+              variant="filled"
+              className={classes.input}
+              {...form.getInputProps('name')}
+            />
+            <TextInput
+              placeholder="Your email"
+              name="email"
+              variant="filled"
+              className={classes.input}
+              {...form.getInputProps('email')}
+            />
+          </SimpleGrid>
+
+          <Textarea
+            mt="md"
+            placeholder="Your message"
+            maxRows={10}
+            minRows={5}
+            autosize
+            name="message"
+            variant="filled"
+            className={classes.input}
+            {...form.getInputProps('message')}
+          />
+          
+
+          <Group justify="center" mt="xl">
+            <Button type="submit" size="md" color="#5b43d6" leftSection={<IconSend/>}>
+              Send message
+            </Button>
+          </Group>
+        </form>
+
+      <hr/>
+
+      <Group className={classes.connect}>
+        <Title size="h4">
+          Connect with me
+        </Title>
+
+        <Box>
+          Let's connect
+          on Linkedin
+        </Box>
+        <Box>
+          Github
+          @githubNickname
+        </Box>
       </Group>
-    </form>
+
+      </Container>
+    </Container>
+    
   );
 }
